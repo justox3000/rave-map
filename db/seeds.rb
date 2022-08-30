c7213d67850ca9f6749fa5fd2ca2328c634e5d4d
Justus Lambrecht <justuslambrecht@icloud.com>
2022-08-30 11:32:09 +0200

    seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,93 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Deleting the old Database..."
+
+Attending.destroy_all
+Comment.destroy_all
+Party.destroy_all
+User.destroy_all
+
+puts "Creating 5 random users..."
+
+user1 = User.new(
+  nickname: "testuser",
+  email: "test@test.com",
+  password: "testtest"
+)
+user1.save!
+
+user2 = User.new(
+  Faker::Internet.user
+)
+
+user3 = User.new(
+  Faker::Internet.user
+)
+
+user4 = User.new(
+  Faker::Internet.user
+)
+
+user5 = User.new(
+  Faker::Internet.user
+)
+
+puts "Create 5 random parties..."
+
+party1 = Party.new(
+  title: Faker::Movies::Tron.tagline,
+  musicgenre: "House",
+  location: "Gewölbe",
+  description: Faker::Lorem.sentence(word_count: 7),
+  date: Faker::Date.forward(days: 23),
+  starttime: "23:00"
+)
+party1.user = user1
+party1.save!
+
+party2 = Party.new(
+  title: Faker::Movies::Tron.tagline,
+  musicgenre: "Techno",
+  location: "Helios 37",
+  description: Faker::Lorem.sentence(word_count: 7),
+  date: Faker::Date.forward(days: 23),
+  starttime: "22:30"
+)
+party2.user = user1
+party2.save!
+
+party3 = Party.new(
+  title: Faker::Movies::Tron.tagline,
+  musicgenre: "Psytrance",
+  location: "Artheater",
+  description: Faker::Lorem.sentence(word_count: 7),
+  date: Faker::Date.forward(days: 23),
+  starttime: "23:30"
+)
+party3.user = user2
+party3.save!
+
+party4 = Party.new(
+  title: Faker::Movies::Tron.tagline,
+  musicgenre: "Techno",
+  location: "Artheater",
+  description: Faker::Lorem.sentence(word_count: 7),
+  date: Faker::Date.forward(days: 23),
+  starttime: "23:30"
+)
+party4.user = user1
+party4.save!
+
+party5 = Party.new(
+  title: Faker::Movies::Tron.tagline,
+  musicgenre: "Techno",
+  location: "Odonien",
+  description: Faker::Lorem.sentence(word_count: 7),
+  date: Faker::Date.forward(days: 23),
+  starttime: "22:30"
+)
+party5.user = user4
+party5.save!
+
+puts "Created 5 users and 5 parties..."
+puts "username: testuser"
+puts "email: test@test.com"
+puts "password: testtest"
