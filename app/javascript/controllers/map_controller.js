294b16a7633fa2633b917cb2c668f7413d14357e
Justus Lambrecht <justuslambrecht@icloud.com>
2022-09-17 15:45:07 +0200

    map controls

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -22,9 +22,60 @@ export default class extends Controller {
       container: this.element,
       // style: 'mapbox://styles/juslambre/cl7ng8x83006615l9wr90ztu0'
       style: 'mapbox://styles/mapbox/dark-v10',
+      cooperativeGestures: true
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
+    this.map.addControl(new mapboxgl.FullscreenControl())
+
+    this.map.on('load', () => {
+      // Insert the layer beneath any symbol layer.
+      const layers = map.getStyle().layers;
+      const labelLayerId = layers.find(
+      (layer) => layer.type === 'symbol' && layer.layout['text-field']
+      ).id;
+
+      // The 'building' layer in the Mapbox Streets
+      // vector tileset contains building height data
+      // from OpenStreetMap.
+      this.map.addLayer(
+      {
+      'id': 'add-3d-buildings',
+      'source': 'composite',
+      'source-layer': 'building',
+      'filter': ['==', 'extrude', 'true'],
+      'type': 'fill-extrusion',
+      'minzoom': 15,
+      'paint': {
+      'fill-extrusion-color': '#aaa',
+
+      // Use an 'interpolate' expression to
+      // add a smooth transition effect to
+      // the buildings as the user zooms in.
+      'fill-extrusion-height': [
+      'interpolate',
+      ['linear'],
+      ['zoom'],
+      15,
+      0,
+      15.05,
+      ['get', 'height']
+      ],
+      'fill-extrusion-base': [
+      'interpolate',
+      ['linear'],
+      ['zoom'],
+      15,
+      0,
+      15.05,
+      ['get', 'min_height']
+      ],
+      'fill-extrusion-opacity': 0.6
+      }
+      },
+      labelLayerId
+      );
+      });
   }
 
   #addMarkersToMap() {
